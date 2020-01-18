ff196c9ad57c21c224658379cede8e83e4d58e4a
Misrudin <37394664+MisrudinNamikaze@users.noreply.github.com>
2020-01-18 21:44:31 +0700

    Add files via upload

---
+++ 6a/index.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Data</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- aswesome -->
+    <link rel="stylesheet" href="css/all.css">
+
+    <!-- google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700|Montserrat:200,400,600&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+<body>
+
+
+ <nav class="navbar">
+  <div class="container">
+         <a class="navbar-brand" href="#">
+            <img src="logo.png" width="70" height="35" alt="logo arkademy">
+          </a>
+        <form class="form-inline frm">
+          <input class="form-control mr-sm-2 txt" type="search" placeholder="Search...." aria-label="Search">
+          <button class="btn btn-warning my-2 my-sm-0 but" type="submit">ADD</button>
+        </form>
+  </div>
+</nav>
+
+
+<section >
+    <table class="table table-borderless view-data">
+  <thead class="bg-warning">
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Cashier</th>
+      <th scope="col">Product</th>
+      <th scope="col">Category</th>
+      <th scope="col">Price</th>
+      <th scope="col">Action</th>    
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Pevita Pearce</td>
+      <td>Latte</td>
+      <td>Drink</td>
+      <td>Rp.10.000</td>
+      <td>
+        <a href="" class="text-success"><i class="fas fa-fw fa-edit"></i></a>
+        <a href="" class="text-danger"><i class="fas fa-fw fa-trash-alt"></i></a>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Raisa Andriana</td>
+      <td>Cake</td>
+      <td>Food</td>
+      <td>Rp.15.000</td>
+      <td>
+        <a href="" class="text-success"><i class="fas fa-fw fa-edit"></i></a>
+        <a href="" class="text-danger"><i class="fas fa-fw fa-trash-alt"></i></a>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Raisa Andriana</td>
+      <td>Fried Rice</td>
+      <td>Food</td>
+      <td>Rp.30.000</td>
+      <td>
+        <a href="" class="text-success"><i class="fas fa-fw fa-edit"></i></a>
+        <a href="" class="text-danger"><i class="fas fa-fw fa-trash-alt"></i></a>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Pevita Pearce</td>
+      <td>Gudeg</td>
+      <td>Food</td>
+      <td>Rp.35.000</td>
+      <td>
+        <a href="" class="text-success"><i class="fas fa-fw fa-edit"></i></a>
+        <a href="" class="text-danger"><i class="fas fa-fw fa-trash-alt"></i></a>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Joko Purwadhi</td>
+      <td>Ice Teh</td>
+      <td>Drink</td>
+      <td>Rp.55.000</td>
+      <td>
+        <a href="" class="text-success"><i class="fas fa-fw fa-edit"></i></a>
+        <a href="" class="text-danger"><i class="fas fa-fw fa-trash-alt"></i></a>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Misrudin</td>
+      <td>Sate Ayam</td>
+      <td>Food</td>
+      <td>Rp.25.000</td>
+      <td>
+        <a href="" class="text-success"><i class="fas fa-fw fa-edit"></i></a>
+        <a href="" class="text-danger"><i class="fas fa-fw fa-trash-alt"></i></a>
+      </td>
+    </tr>
+  </tbody>
+</table>
+</section>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/all.js"></script>
+  </body>
+</html>
